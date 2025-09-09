f5d554dc02e20df7a723dd9920d8fd696a156745
cristianv.ms <c71917455@gmail.com>
2025-09-09 15:07:00 -0500

    testt

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! CI/CD';
+    return 'Hello World! CI/CD 2025';
   }
 }
