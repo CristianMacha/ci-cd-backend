81b6ad6a378a01dab7dd540de0edff5273949d50
cristianv.ms <c71917455@gmail.com>
2025-09-09 20:37:05 -0500

    teste

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('CI/CD rc2');
+      expect(appController.getHello()).toBe('CI/CD rc22');
     });
   });
 });
